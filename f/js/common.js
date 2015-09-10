45caf07908921710e25161e4c83ed6d0fb51c02d
BerTepesh <Ber.Tepesh@gmail.com>
2015-09-10 17:58:18 +0300

    +

--- f/js/common.js
+++ f/js/common.js
@@ -10,4 +10,20 @@ $(function(){
 		sw.find(".switcher__holder").eq(index).addClass("switcher__holder_active")
 		return false;
 	});
+	$(".js-scroll-link").click(function(){
+  	if(!$(this).hasClass("active")) {
+	    var scrollTo = $(this).data("scrollto"),
+	    scrollTarget = $('*[data-scrollto="'+scrollTo+'"].js-scroll-target');
+	    $.scrollTo(scrollTarget, 350, {offset: 0});
+  	}
+  });
+});
+$(document).ready(function(){
+	$(".brand .brand__item").each(function(){
+		var hidden = $(this).parents(':hidden');
+		hidden.css("opacity", "0.01").show();
+		var width = $(this).width();
+		hidden.hide().css("opacity", "1");
+		$(this).height(width);
+	});
 });
